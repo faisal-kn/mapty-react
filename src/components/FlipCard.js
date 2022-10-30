7705a052447f473bfdd277e858542b14481f0561
faisal-kn <khanmohdfaisal75@gmail.com>
2022-10-30 21:41:35 +0530

    home page css fixed

--- src/components/FlipCard.js
+++ src/components/FlipCard.js
@@ -1,24 +1,27 @@
-import React from 'react'
-import "../css/flipCard.css"
-
+import React from "react";
+import "../css/flipCard.css";
 
 function FlipCard(props) {
-    return (
-        <div>
-            <div class="flip-card">
-                <div class="flip-card-inner">
-                    <div class="flip-card-front">
-                        <img alt="" src={props.src} style={{height: "250px", decoding:"async", dataNimg:"fill"}}
-                        className="w-full rounded-lg card-img-top"/>
-                    </div>
-                    <div class="flip-card-back" style={{}}>
-                        <h2>{props.h2}</h2> 
-                        <p style={{marginTop:"20px"}} >{props.p}</p> 
-                    </div>
-                </div>
-            </div>
+  return (
+    <div>
+      <div className="flip-card">
+        <div className="flip-card-inner ">
+          <div className="flip-card-front" style={{ padding: "40px" }}>
+            <img
+              alt=""
+              src={props.src}
+              style={{ height: "250px", decoding: "async", dataNimg: "fill" }}
+              className="w-full rounded-lg card-img-top"
+            />
+          </div>
+          <div className="flip-card-back p-3" style={{}}>
+            <h2>{props.h2}</h2>
+            <p style={{ marginTop: "20px" }}>{props.p}</p>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default FlipCard
+export default FlipCard;
--- src/components/StoryCard.js
+++ src/components/StoryCard.js
@@ -1,22 +1,32 @@
-import React from 'react'
-import "../css/StoryCard.css"
+import React from "react";
+import "../css/StoryCard.css";
 
-function StoryCard(props) {
+function StoryCard({ src, p, h5 }) {
   return (
     <div>
-            <div class = "box">
-            <div class = "card">
-                <div class = "image">
-                <img href = "#" alt="" src ={props.src} style={{height:"100px" ,width:"260px"}}/>
-                </div>
-            <div class ="content">
-                <h5 class="card-title" style={{textAlign:"center" , color:"rgba(17, 91, 230, 0.712)"}}>{props.h5}</h5>
-                <p class="card-text">{props.p}.</p>
-                </div>
-            </div>    
-            </div>
+      <div class="box">
+        <div class="card">
+          <div class="image">
+            <img
+              href="#"
+              alt=""
+              src={src}
+              style={{ height: "100px", width: "260px" }}
+            />
+          </div>
+          <div class="content">
+            <h5
+              class="card-title"
+              style={{ textAlign: "center", color: "rgba(17, 91, 230, 0.712)" }}
+            >
+              {h5}
+            </h5>
+            <p class="card-text">{p}.</p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default StoryCard
+export default StoryCard;
